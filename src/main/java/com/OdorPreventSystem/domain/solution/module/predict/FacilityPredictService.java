597555d1febb37bb64325e659d126c20d3d5fc8c
dldsudqlsdlqslek <tina4686@gmail.com>
2024-12-09 20:07:18 +0900

    predict 탭 시설 상세 정보 추가

--- src/main/java/com/OdorPreventSystem/domain/solution/module/predict/FacilityPredictService.java
+++ src/main/java/com/OdorPreventSystem/domain/solution/module/predict/FacilityPredictService.java
@@ -13,7 +13,7 @@ public interface FacilityPredictService {
 	List<FacilityDto.Entity> selectFacilityList(); //시설리스트 불러옴
 	PredictFacilityDto.Response initPredictDto(); // 예측설비 응답객체 초기화
 	List<FacilityCombination> findCombinations(UserInputDataDto.Response inputdata, PredictFacilityDto.Response facility);//입력 데이터 기반 시설 조합 찾기 
-	List<String> getImages(int index); //이미지 가져옴
+	List<FacilityDto.CombinationResponse> getImageAndFacs(int index); //이미지 가져옴
 	FacilityCombination getCombination(int i); //조합 객체 반환
 	void saveData(Response facility, Request facilityreq);//저장
 	void updateFacility(); //업데이트
--- src/main/resources/static/solution/js/predict.js
+++ src/main/resources/static/solution/js/predict.js
@@ -31,39 +31,38 @@ function handleClick(event) {
   var texts = clicked.innerText.split(".");
   //console.log(texts);
 
-  //서버로 전송해서 이미지파일 리스트 요청
+  //서버로 전송해서 이미지파일 및 상세정보 리스트 요청
   $.ajax({
     type: "post",
     url: "/soms/page5/predict?i=" + parseInt(texts[1], 10),
-    dataType: "text",
-    success: function (imagelist) {
+    dataType: "json",
+    success: function (response) {
       //성공 시 이미지 테이블 비우고
       $("#image-facility").empty();
       //console.log(imagelist);
 
       //이미지파일 리스트 각각 요소에 대해
-      $.each(JSON.parse(imagelist), function (index, item) {
-        //동적 테이블 생성
+      response.forEach(function (facility)  {
 
-        var facImg = '<div class="comb-image">'; // 이미지와 표를 감싸는 div 생성
+        let facilityHtml = '<div class="comb-image">';
 
-        //코드는 디자인상 필요하시면 바꾸시면 됩니다. 리스트 인덱스 필요하면 위에 function(index, item)에 index 쓰세요
+        // 이미지 추가
+        facilityHtml += '<img src="/static/solution/img/' + facility.imagesrc + '"';
+        facilityHtml +=
+            'alt="No image" class="image_left" style="width: 100%; height: 55%; padding: 10px 0px;" ';
+        facilityHtml += 'onclick="window.open(this.src)">';
 
-        // 이미지 요소 추가
-        facImg += '<img src="/static/solution/img/' + item + '"';
-        facImg +=
-          'alt="No image" class="image_left" style="width: 100%; height: 55%; padding: 10px 0px;" ';
-        facImg += 'onclick="window.open(this.src)">';
+        // 시설 상세 정보 표시
+        // 시설 정보 추가 표시 필요하면 json 수정 후 추가 가능
+        facilityHtml += '<div class="facility-state">';
+        facilityHtml += "<p>시설 이름: " + facility.name + "</p>";
+        facilityHtml += "<p>시설 종류: " + facility.category + "</p>";
+        facilityHtml += "<p>시설 가격: " + facility.cost + "</p>";
+        facilityHtml += "</div>";
 
-        // 시설 상태 정보를 포함하는 표 추가
-        facImg += '<div class="facility-state">';
+        facilityHtml += "</div>";
 
-        facImg += "<p>시설 상태 정보가 없습니다</p>"; // 시설 상태 정보 값 넣어줄 것. //
-
-        facImg += "</div>"; // facility-state div 닫기
-        facImg += "</div>"; // comb-image div 닫기
-
-        $("#image-facility").append(facImg); //id="image-facility" 요소 안에 facImg 추가
+        $("#image-facility").append(facilityHtml); //id="image-facility" 요소 안에 facilityHtml 추가
       });
       // 로딩 스피너 숨기기
       $("#loading-spinner").hide();
